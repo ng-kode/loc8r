97e0e6a169cb1b30863d88648d7d612d4b1742ef
Richard-NG <richard.chihong.ng@gmail.com>
2018-01-26 22:57:07 +0800

    Consuming API GET /locations/:locationid

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -10,6 +10,7 @@ if (process.env.NODE_ENV === 'production') {
 
 const renderHomepage = (req, res, body) => {
     res.render('locations-list', {
+        _id: body._id,
         title: 'Loc8r - find a place to work with wifi',
         pageHeader: {
           title: 'Loc8r',
@@ -39,49 +40,33 @@ const homelist = (req, res, next) => {
     })
 };
 
-const locationInfo = (req, res, next) => {
+const renderDetailPage = (req, res, body) => {
     res.render('location-info', {
-        name: 'Starcups',
-        address: '125 High Street, Reading, RG6 1PS',
+        name: body.name,
+        address: body.address,
         rating: 3,
-        facilities: [ 'Hot drinks', 'Food', 'Premium wifi'],
-        distance: '100m',
-        openingTimes: [
-            {
-                days: 'Monday - Friday',
-                opening: '7:00am',
-                closing: '7:00pm',
-                closed: false
-            },
-            {
-                days: 'Saturday',
-                opening: '8:00am',
-                closing: '5:00pm',
-                closed: false
-            },
-            {
-                days: 'Sunday',
-                closed: true
-            },
-        ],
-        mapLink: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14764.396703283839!2d114.18555039751222!3d22.31208868261436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x340400e196516635%3A0x3fcd21c76c5a5ca4!2zU3RhcmJ1Y2tzIENvZmZlZSDmmJ_lt7TlhYs!5e0!3m2!1sen!2shk!4v1516712487947",
-        reviews: [
-            {
-                rating: 5,
-                author: "Samantha Thavasa",
-                timestamp: "23 January 2018",
-                text: "Great great place to have a chat with Victoria Harbour near you !"
-            },
-            {
-                rating: 3,
-                author: "Ted Baker",
-                timestamp: "11 July 2017",
-                text: "Coffee as crappy as usual by the view is nice"
-            }
-        ],
+        facilities: body.facilities,
+        openingTimes: body.openingTimes,
+        coords: { lng: body.coords[0], lat: body.coords[1] },
+        reviews: body.reviews,
         summaryLead: "Simon's cafe is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.",
         summary: "If you've been and you like it - or if you don't - please leave a review to help other people just like you."
     })
+}
+
+const locationInfo = (req, res, next) => {
+    const options = {
+        url: apiOptions.server + 'api/locations/' + req.params.locationid,
+        method: "GET",
+        json: {},
+        query: {}
+    }
+    request(options, (err, response, body) => {
+        if (err) { return utils.customError(err, res) }
+        if (response.statusCode != 200) { return utils.customError(body, res, false) }
+        return renderDetailPage(req, res, body)
+    })
+
 };
 
 const addReview = (req, res, next) => {
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -6,8 +6,8 @@ const ctrlOthers = require('../controllers/others');
 
 /* Locations pages. */
 router.get('/', ctrlLocations.homelist);
-router.get('/location', ctrlLocations.locationInfo);
-router.get('/location/review/new', ctrlLocations.addReview);
+router.get('/locations/:locationid', ctrlLocations.locationInfo);
+router.get('/locations/:locationid/reviews/new', ctrlLocations.addReview);
 
 
 router.get('/about', ctrlOthers.about);
