78cc57a590a0a2249473672f499c2443ebd18e16
Richard-NG <richard.chihong.ng@gmail.com>
2018-01-30 00:02:29 +0800

    Added simple routes on Angular side

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -2,23 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 
 import { HomeListComponent } from './home-list/home-list.component';
 import { DistancePipe } from './distance.pipe';
+import { FrameworkComponent } from './framework/framework.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
     HomeListComponent,
-    DistancePipe
+    DistancePipe,
+    FrameworkComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+        {
+            path: '',
+            component: HomeListComponent
+        },
+        {
+            path: 'about',
+            component: AboutComponent
+        }
+    ])
   ],
   providers: [HttpClientModule],
-  bootstrap: [HomeListComponent]
+  bootstrap: [FrameworkComponent]
 })
 export class AppModule { }
